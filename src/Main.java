d3fff08f5d5a9257e697f08d0656b0534b62f756
plesen666 <diada.var@mail.ru>
2024-06-18 23:08:27 +0300

    HW-2 step4 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-    var dog=8.0;
-    var cat=3.6;
-    var paper=763789;
-dog=dog - 3.5;
-cat=cat - 1.6;
-paper=paper -7639;
+    var friend=19;
 
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
+        System.out.println(friend);
     }
 }
