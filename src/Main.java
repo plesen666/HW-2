2bf15fa71585ff2134cd79505dc2f34dfd6041c0
plesen666 <diada.var@mail.ru>
2024-06-18 23:12:29 +0300

    HW-2 step4 2

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
     var friend=19;
-        friend= friend + 2;
+     var sumFriend= friend + 2;
+        var divideFriend=19/7;
 
         System.out.println(friend);
+        System.out.println(sumFriend);
+        System.out.println(divideFriend);
     }
 }
