8ecbee358f7a39d0e07f29382971dc31be89be40
plesen666 <diada.var@mail.ru>
2024-06-19 00:08:34 +0300

    step4 1

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,18 @@ public class Main {
         var dog=8.0;
         var cat=3.6;
         var paper=763789;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        dog=dog + 4.0;
-        cat=cat + 4.0;
-        paper=paper + 4;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-
         dog=dog - 3.5;
         cat=cat - 1.6;
         paper=paper -7639;
+        var friend=19;
 
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        System.out.println(friend);
+
+
+
     }
 }
 
