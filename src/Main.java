7692b4829f06526de6c97ba383f14ff028853c02
plesen666 <diada.var@mail.ru>
2024-06-19 00:05:15 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,20 @@ public class Main {
     public static void main(String[] args) {
 
 
-                var dog=8.0;
-                var cat=3.6;
-                var paper=763789;
+        var dog=8.0;
+        var cat=3.6;
+        var paper=763789;
+
+        dog=dog + 4.0;
+        cat=cat + 4.0;
+        paper=paper + 4;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.print(paper);
+        System.out.println(paper);
+    }
+}
 
 
-                System.out.println(dog);
-                System.out.println(cat);
-                System.out.print(paper);
-            }
-        }
 
