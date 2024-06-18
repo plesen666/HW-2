540ef83d5bf74791adcb2995f0bc04f9f7c448dc
plesen666 <diada.var@mail.ru>
2024-06-18 23:58:41 +0300

    6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        var friend=19;
+        var sumFriend= friend + 2;
+        var divideFriend=19/7;
+
+        System.out.println(friend);
+        System.out.println(sumFriend);
+        System.out.println(divideFriend);
         var frog =3.5;
         System.out.println(frog);
         frog = frog * 10;
