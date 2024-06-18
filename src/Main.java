eead969dc385c0cf51537d8503c13735694b0996
plesen666 <diada.var@mail.ru>
2024-06-18 23:16:27 +0300

    HW-2 step5

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-    var friend=19;
-     var sumFriend= friend + 2;
-        var divideFriend=19/7;
-
-        System.out.println(friend);
-        System.out.println(sumFriend);
-        System.out.println(divideFriend);
+    var frog =3.5;
+        System.out.println(frog);
+         frog = frog * 10;
+        System.out.println(frog);
+        frog = frog/3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
     }
 }
